a3df505d6dec7096493ee3aae2ab78f033463639
M_Taniguro <48035737+mTaniguro@users.noreply.github.com>
2021-08-20 10:03:32 -0700

    Add files via upload

---
+++ ShakeMeSilly/ShakeMeSilly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShakeMeSilly
+//
+//  Created by mami taniguro on 8/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
